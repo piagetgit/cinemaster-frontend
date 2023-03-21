1636aaf0c09f518974d12a3647794e35c0c3cbd6
Pitt <pietro.godone@gmail.com>
2023-03-21 22:23:36 +0100

    Ho cominciato ad ispirarmi al lavoro della prof, ma mancano i servizi, il css e i js a cui legarsi ovviamente

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppStateService} from "../app-state.service";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  view!: string;
 
+  constructor(private appServ: AppStateService) {
+    this.view = this.appServ.currentView;
+  }
+
+  ngOnInit(): void {
+    this.appServ.observe("view", (view: string) => {
+      this.view = view;
+    });
+  }
+
+  goTo(where: "home" | "user" | "film" | "tickets") {
+    this.appServ.currentView = where;
+  }
 }
