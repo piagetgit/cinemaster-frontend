dfb94f6b1c31d24837d055e5ab8493afa4a41319
Pitt <pietro.godone@gmail.com>
2023-03-28 22:20:27 +0200

    Template base Tickets ma senza funzione buy

--- src/app/tickets/tickets.component.ts
+++ src/app/tickets/tickets.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppStateService } from "../services/app-state.service";
+import {AppService} from "../services/app.service";
 import {FilmInfoI} from "../interface/film";
 import {UserInfoI} from "../interface/userLoginResponse";
+import {DateAdapter} from "@angular/material/core";
 
 @Component({
   selector: 'app-tickets',
@@ -9,12 +11,39 @@ import {UserInfoI} from "../interface/userLoginResponse";
   styleUrls: ['./tickets.component.css']
 })
 export class TicketsComponent implements OnInit{
-  films!:FilmInfoI[];
-  user!:UserInfoI;
+  films!:FilmInfoI[] | null;
+  film!:FilmInfoI;
+  titolo!: string;
+  nSeats!: string;
+  tickets!: {nome: string, seats: number, price: number}[];
 
-  constructor(private appStateService:AppStateService) {
+  constructor(private appService:AppService){
+
   }
 
   ngOnInit() {
+    this.appService.films.then((films)=>{
+      this.films=films;
+    });
+  }
+
+  verifyFilm(event: Event) {
+    this.titolo = (event.target as HTMLInputElement).value;
+    if (this.films !== null) {
+      for (let film of this.films) {
+        if (film.titolo === this.titolo)
+          this.film = film;
+      }
+    }
+  }
+
+  addSeats(event: Event) {
+    this.nSeats = (event.target as HTMLInputElement).value;
+    const posti = parseInt(this.nSeats, 10);
+  }
+
+  buy(event: MouseEvent) {
+    event.preventDefault();
+
   }
 }
